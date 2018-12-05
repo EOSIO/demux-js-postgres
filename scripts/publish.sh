67a0fbf08d8696f08c5511aabfd586b6159321bd
Julien Heller <julien.heller@block.one>
2018-12-05 09:59:43 -0500

    Fix whitespace

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,10 +7,10 @@ setup_git() {
   git config --global user.email "devops@block.one"
   git config --global user.name "blockone-devops"
   git config --global push.default matching
-  
+
   # Get the credentials from a file
   git config credential.helper "store --file=.git/credentials"
-  
+
   # This associates the API Key with the account
   echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
 }
@@ -21,9 +21,9 @@ make_version() {
   # 1. package-lock.json is not aligned with package.json
   # 2. npm install is run
   git checkout -- .
-  
+
   # Echo the status to the log so that we can see it is OK
-  git status 
+  git status
 
   # Run the deploy build and increment the package versions
   current_commit="$(git rev-parse --short HEAD)";
@@ -44,6 +44,6 @@ make_version
 
 echo "Publish to NPM"
 
-cp .npmrc.template $HOME/.npmrc 
+cp .npmrc.template $HOME/.npmrc
 
 npm publish --tag edge
