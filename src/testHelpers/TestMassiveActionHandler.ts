93a9048bc0bd9d56e62ffcac5830691fcf17fe04
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2019-01-09 13:02:42 -0500

    Add linting and fix errors

--- src/testHelpers/TestMassiveActionHandler.ts
+++ src/testHelpers/TestMassiveActionHandler.ts
@@ -1,10 +1,10 @@
-import { MassiveActionHandler } from "../MassiveActionHandler"
+import { MassiveActionHandler } from '../MassiveActionHandler'
 
 export class TestMassiveActionHandler extends MassiveActionHandler {
   public reset() {
     this.lastProcessedBlockNumber = 0
-    this.lastProcessedBlockHash = ""
-    this.handlerVersionName = "v1"
+    this.lastProcessedBlockHash = ''
+    this.handlerVersionName = 'v1'
   }
   public async _loadIndexState() {
     return await this.loadIndexState()
