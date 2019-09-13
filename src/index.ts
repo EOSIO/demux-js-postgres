8ebae7b43357363ca96b21aca808fe8910582489
Utkarsh Gupta <utkarshpramodgupta@gmail.com>
2019-09-13 17:26:44 +0530

    Fixed syntax for CommonJS module exporting/importing.
    `massive` is written in CommonJS and not Typescript. Exporting it like `export {default as massive} from 'massive'` would export massive with `undefined` value.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+declare function require(name:string):any;
+const massive = require('massive');
+export { massive }
 export { MassiveActionHandler } from './MassiveActionHandler'
 export { Migration } from './Migration'
 export { MigrationRunner } from './MigrationRunner'
 export { MigrationSequence, MassiveActionHandlerOptions } from './interfaces'
-export { default as massive } from 'massive'
